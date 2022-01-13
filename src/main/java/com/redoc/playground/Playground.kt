5ea2efcc8b2c68d790139e123027be88d7d8f204
RedoC-github <coderlovemedia@gmail.com>
2022-01-13 21:05:48 +0900

    EventHandler + Emergency Maintenance System

--- src/main/java/com/redoc/playground/Playground.kt
+++ src/main/java/com/redoc/playground/Playground.kt
@@ -1,12 +1,17 @@
 package com.redoc.playground
-
 import org.bukkit.plugin.java.JavaPlugin
+
+/**
+ * @author RedoC
+ * */
 
 class Playground : JavaPlugin() {
     override fun onEnable() {
+        println("§aWelcome to RedoC's Playground")
         server.apply {
             pluginManager.registerEvents(PluginListener(), this@Playground)
         }
+
     }
 
     override fun onDisable() {
--- src/main/java/com/redoc/playground/PluginListener.kt
+++ src/main/java/com/redoc/playground/PluginListener.kt
@@ -1,8 +1,24 @@
 package com.redoc.playground
 
+import org.bukkit.ChatColor
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.player.PlayerMoveEvent
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerQuitEvent
+
+/**
+ * @author RedoC
+ * */
 
 class PluginListener: Listener {
+    @EventHandler
+    fun onJoin(event: PlayerJoinEvent) {
+        event.player.kickPlayer("§4§lEMERGENCY MAINTENANCE§r\n" + "긴급 서버 점검중입니다. 불편을 드려 죄송합니다.")
+        event.joinMessage = "§a[SERVER]§r " + "Welcome, " + event.player.name
+    }
+
+    @EventHandler
+    fun onQuit(event: PlayerQuitEvent) {
+        event.quitMessage = "§a[SERVER]§r " + "Bye, " + event.player.name
+    }
 }
