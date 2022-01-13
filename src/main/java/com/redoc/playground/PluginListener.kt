528883670373a685753bd0fdb5e9c3c6243bb17b
RedoC <coderlovemedia@gmail.com>
2022-01-13 21:10:32 +0900

    Update PluginListener.kt

--- src/main/java/com/redoc/playground/PluginListener.kt
+++ src/main/java/com/redoc/playground/PluginListener.kt
@@ -11,14 +11,4 @@ import org.bukkit.event.player.PlayerQuitEvent
  * */
 
 class PluginListener: Listener {
-    @EventHandler
-    fun onJoin(event: PlayerJoinEvent) {
-        event.player.kickPlayer("§4§lEMERGENCY MAINTENANCE§r\n" + "긴급 서버 점검중입니다. 불편을 드려 죄송합니다.")
-        event.joinMessage = "§a[SERVER]§r " + "Welcome, " + event.player.name
-    }
-
-    @EventHandler
-    fun onQuit(event: PlayerQuitEvent) {
-        event.quitMessage = "§a[SERVER]§r " + "Bye, " + event.player.name
-    }
 }
